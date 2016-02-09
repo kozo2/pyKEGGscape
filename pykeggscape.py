f2a62ea0c4de96ee7d464e4a40968a5cbc84e83d
Kozo Nishida <kozo.nishida@gmail.com>
2016-02-09 21:42:46 +0900

    Create pykeggscape.py

---
+++ pykeggscape.py
@@ -0,0 +1,6 @@
+from Bio.KEGG.REST import *
+from Bio.KEGG.KGML import KGML_parser
+
+def get_pathway(identifier, organism):
+    """Request KEGG API and visualize KEGG pathway with Cytoscape."""
+    pass
